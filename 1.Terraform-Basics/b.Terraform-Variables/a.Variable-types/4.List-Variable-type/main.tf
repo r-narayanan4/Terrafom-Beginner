0200ee347b68b11e2aeba890587ef4c3c5b4344b
rln <rln@gmail.com>
2024-03-09 16:04:04 +0530

    List-variable-type

---
+++ 1.Terraform-Basics/b.Terraform-Variables/a.Variable-types/4.List-Variable-type/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    region     = "ap-south-2"
+    access_key = "your-access-key"
+    secret_key = "your-secret-key"
+} 
+
+
+resource "aws_instance" "ec2_example" {
+    ami           = "ami-0767046d1677be5a0"
+    instance_type =  "t3.micro"
+    count = 1
+
+    tags = {
+        Name = "Terraform EC2"
+        }
+
+}
+
+resource "aws_iam_user" "example" {
+    count = length(var.user_names)
+    name  = var.user_names[count.index]
+}
