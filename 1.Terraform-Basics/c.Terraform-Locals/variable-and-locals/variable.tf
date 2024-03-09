dc1835ce1579cd4444306becc7cb276063e0ff99
rln <rln@gmail.com>
2024-03-09 20:56:50 +0530

    terraform-locals

---
+++ 1.Terraform-Basics/c.Terraform-Locals/variable-and-locals/variable.tf
@@ -0,0 +1,11 @@
+variable location {
+    description = "Location of server"
+    type        = string
+    default     = "Tamil Nadu"
+}
+
+variable server_name {
+    description = "Name of server"
+    type        = string
+    default     = "primary-app-server"
+}
